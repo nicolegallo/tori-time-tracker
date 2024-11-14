e10d659233e792252bd65ae92dd1ae7b6d9a9de2
Nicole Gallo <40307517+nicolegallo@users.noreply.github.com>
2024-11-14 12:09:32 -0500

    add js timer for time since we've started talking

--- script.js
+++ script.js
@@ -190,4 +190,31 @@ document.addEventListener("DOMContentLoaded", function() {
     myToriTimeHeader.addEventListener("click", function() {
         logContainer.style.display = logContainer.style.display === "none" ? "block" : "none";
     });
+
+    var startDateTime = new Date(2024,8,8,19,27,0,0); // YYYY (M-1) D H m s (start time and date from DB)
+var startStamp = startDateTime.getTime();
+
+var newDate = new Date();
+var newStamp = newDate.getTime();
+
+var timer;
+
+function updateClock() {
+    newDate = new Date();
+    newStamp = newDate.getTime();
+    var diff = Math.round((newStamp-startStamp)/1000);
+    
+    var d = Math.floor(diff/(24*60*60));
+    diff = diff-(d*24*60*60);
+    var h = Math.floor(diff/(60*60));
+    diff = diff-(h*60*60);
+    var m = Math.floor(diff/(60));
+    diff = diff-(m*60);
+    var s = diff;
+    
+    document.getElementById("time-elapsed").innerHTML = d+" days, "+h+" hours, "+m+" minutes, "+s+" seconds";
+}
+
+setInterval(updateClock, 1000);
 });
+
